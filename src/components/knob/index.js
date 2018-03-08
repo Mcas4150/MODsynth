b052ae4b40d42982b9541338c25b54482b53b85a
Michael Cassidy <mikecassidy0000@gmail.com>
2018-03-07 19:23:39 -0500

    knobs

---
+++ src/components/knob/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './knob.css';
+
+
+
+class Knob extends Component {
+  render() {
+    return (
+      <div className="knob flex-center">
+
+
+      </div>
+    );
+  }
+}
+
+export default Knob;
--- src/components/modDUO/index.js
+++ src/components/modDUO/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './modDUO.css';
 import Screen from '../screen/index';
+import Knob from '../knob/index';
 
 class ModDUO extends Component {
   render() {
@@ -12,8 +13,12 @@ class ModDUO extends Component {
           <div className="r-screen">
             <Screen/>
           </div>
-          <div className="l-knob"></div>
-          <div className="r-knob"></div>
+          <div className="l-knob">
+            <Knob/>
+          </div>
+          <div className="r-knob">
+            <Knob/>
+          </div>
           <div className="handle"></div>
           <div className="l-button"></div>
           <div className="r-button"></div>
